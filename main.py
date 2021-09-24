763ddf0351756cd75e4902a8c4476198d2b6cf07
Mr. James <68863931+jisforjt@users.noreply.github.com>
2021-09-24 10:58:02 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+# CircuitPython CLUE main.py
+# Last upated: Aug. 11, 2020, v1.0
+# Author(s): James Tobin
+
+######################################################
+#   Version Notes
+######################################################
+'''
+v1.0
+    - Run a specific file by importing it.
+    - You can add more imports and run a list of programs. 
+      Just remember each file needs to end to run the next one.
+'''
+######################################################
+#   Import
+######################################################
+
+#Uncomment the version you are using
+#import menu
+#import menu_lite
+#import menu_cutebot
+#import bluefruitconnect_cutebot_controlpad
+#import cutebot_simple_test
+#import clue_display_sensor_data
+#import cutebot_simple_avoidance
+#import cutebot_IR_remote
+#import cutebot_line_following__simple__
+#import cutebot_line_following__better__
+#import clue_display_sensor_data
+#import gesture_test
+#import clue_simpletest
+#import clue_compass
+#import BLE_detailed_scan
+#import BLE_connect
+#import BLE_bleio
+#import BLE_HID_host
+#import BLE_HID_Central_demo
+#import adafruit_uart_pin_finder
+#import circuitpython_uart
+#import image_transfer
+#import cutebot_joust
+#import spooky_eyes
+#import jisforjt_yahboom_gamepad_test
+#import mic_test
+#import notes_basic
+#import notes_fft
+#import notes_fft_game
+#import move_mouse
+#import jisforjt_egg_quest
+#import time_test
+#import vrbox_connect
+#import BLE_test_connect
+#import servokit_test
+#import clue_servo_test
+import jisforjt_QT_Py_Duo_Pop
+
+print("END - main.py")
